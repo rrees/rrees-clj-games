3c47a84d08ff4d4b27aa4ca496022007e3490467
Robert Rees <Robert Rees>
2012-08-29 23:30:34 +0100

    Creates a working display panel

--- snakes/project.clj
+++ snakes/project.clj
@@ -1,3 +1,4 @@
 (defproject snakes "0.0.1-SNAPSHOT"
   :description "Following the Snake example in Programming Clojure"
-  :dependencies [[clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main snakes.core)
--- snakes/src/snakes/core.clj
+++ snakes/src/snakes/core.clj
@@ -1,1 +1,32 @@
-(ns snakes.core)
+(ns snakes.core
+	(:import
+		(javax.swing JPanel JFrame Timer)
+		(java.awt Dimension)
+		(java.awt.event ActionListener KeyListener)))
+
+(def width 400)
+(def height 400)
+
+(defn game-panel []
+	(proxy [JPanel KeyListener] []
+		(paintComponent [g]
+			(.drawString g "Hello world" 50 50))
+
+		(keyPressed [e])
+		(keyReleased [e])
+		(keyTyped [e])
+
+		(getPreferredSize []
+			(Dimension. width height))))
+
+(defn -main [& args]
+	(let [frame (JFrame. "Snake")
+		panel (game-panel)]
+	(doto panel
+		(.setFocusable true)
+		(.addKeyListener panel))
+	(doto frame
+		(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+		(.add panel)
+		(.pack)
+		(.setVisible true))))
